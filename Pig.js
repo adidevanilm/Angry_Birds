53120f10f66da11160f1a2aff9b24734e23ef621
adidevanilm <76687478+adidevanilm@users.noreply.github.com>
2021-03-30 06:12:19 -0400

    Update Pig.js

--- Pig.js
+++ Pig.js
@@ -2,6 +2,7 @@ class Pig extends BaseClass {
   constructor(x, y){
     super(x,y,50,50);
     this.image = loadImage("sprites/enemy.png");
+    this.image2 = loadImage("clipart633135.png");
     this.Visibility = 255;
   }
     display(){
@@ -14,7 +15,7 @@ class Pig extends BaseClass {
         this.Visibility = this.Visibility-2;
         this.body.position.y = this.body.position.y-3
         tint (255,this.Visibility)
-        image(this.image,this.body.position.x, this.body.position.y, 50, 50)
+        image(this.image2,this.body.position.x, this.body.position.y, 50, 50)
         pop ()
       }
  
